12a287c0a32d92602a75158b225cf68b7d42dc06
Dan Zera <dczera@gmail.com>
2020-03-10 21:47:54 -0700

    save deck of cards to file and add method to load file

--- lib/cards.ex
+++ lib/cards.ex
@@ -40,4 +40,22 @@ defmodule Cards do
 		# will return a Tuple { [hand], [remaining_deck] }
 		Enum.split(deck, num_cards)
 	end
+
+	def save(deck, filename) do
+		# Elixir sits on top of Erlang, so Erlang code can be freely called as well
+		# Erlang code invoked by using the Atom (constant) :erlang which has many built in methods
+		binary = :erlang.term_to_binary(deck)
+		File.write(filename, binary)
+	end
+
+	def load(filename) do
+		{ status, binary } = File.read(filename) # will throw argument error if filename doesn't correspond to an existing file
+
+		# catch any errors using a case statement
+		case status do
+			:ok -> :erlang.binary_to_term binary
+			:error -> "Ah, ah, ah, you didn't say the magic word."
+		end
+		
+	end
 end
